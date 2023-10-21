7266380774c548fd5c5eea2cc0448840294bd359
Harjas123 <harjasandhu@gmail.com>
2023-10-21 13:18:43 -0500

    Combined day01 parts 1 and 2 into a single file.

--- 2022/day01.py
+++ 2022/day01.py
@@ -1,14 +1,17 @@
-
-elves = []
+max = 0
 current = 0
+elves = []
 
-with open('2022/input/day1.txt', "r") as file:
+with open('2022/input/day01.txt', "r") as file:
     for line in file:
         if line.strip():
             current += int(line)
         else:
             elves.append(current)
+            if current > max:
+                max = current
             current = 0
 
+print(max)
 elves.sort(reverse=True)
 print(sum(elves[:3]))
